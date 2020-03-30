7174b9f8eb7150ba3cdbe5f90425c66193e0df3b
Ryan Parrish <rino3717@gmail.com>
2020-03-30 21:45:16 +0000

    Done.

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,40 @@
-
+class Book
+  # give each new book created a title immediately 
+  def initialize(title)
+    @title = title
+  end
+  # What is the actual title that was given to the new book?
+  def title
+    @title
+  end
+  
+  def author=(author)
+    @author = author
+  end
+  
+  def author
+    @author
+  end
+  
+  def page_count=(count)
+    @page_count = count 
+  end
+  
+  def page_count
+    @page_count
+  end
+  
+  def genre=(theme)
+    @genre = theme
+  end
+  
+  def genre
+    @genre 
+  end
+  
+  def turn_page
+    puts "Flipping the page...wow, you read fast!"
+  end
+    
+end
 
--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,28 @@
-# Make your shoe class here!
+class Shoe
+  def initialize(brand)
+    @brand = brand
+  end
+  
+  def brand
+    @brand
+  end
+  
+  def color=(color)
+    @color = color
+  end
+  
+  def color 
+    @color
+  end
+  
+  attr_accessor :size
+  attr_accessor :material
+  attr_accessor :condition
+  
+  def cobble
+    @cobble
+    @condition = "new"
+    
+    puts "Your shoe is as good as new!"
+  end
+end
