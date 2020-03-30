75b0b7fcd19c6a7d0bca9992fd0baabc3fd1f4a6
Ryan Parrish <rino3717@gmail.com>
2020-03-30 21:40:50 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -17,12 +17,10 @@ class Shoe
   
   attr_accessor :size
   attr_accessor :material
-  attr_accessor :condition(="new")
+  attr_accessor :condition
   
   def cobble
     @cobble
     puts "Your shoe is as good as new!"
-    
-    
   end
 end
